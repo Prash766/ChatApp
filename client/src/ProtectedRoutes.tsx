e2ec9289fe1127240fba6def837495e10598b614
Prash766 <prashantshukla766@gmail.com>
2024-12-09 01:16:25 +0530

    implemented function call to validate the auth status

--- client/src/ProtectedRoutes.tsx
+++ client/src/ProtectedRoutes.tsx
@@ -1,13 +1,21 @@
-import { useEffect } from "react"
-import { useAuthStore } from "./store/useAuthStore"
-import { Navigate, Outlet } from "react-router-dom"
+import { useEffect } from "react";
+import { useAuthStore } from "./store/useAuthStore";
+import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = () => {
-    const {isAuthenticated  , authenticateUser} = useAuthStore()
-    useEffect(()=>{
-     authenticateUser()
-    },[])
-    return isAuthenticated ?<Outlet/> : <Navigate to= '/login' replace={true}/>
-}
+  const { isAuthenticated, isLoading, authenticateUser } = useAuthStore();
 
-export default ProtectedRoutes
+  useEffect(() => {
+    const checkAuth = async () => {
+      await authenticateUser();
+    };
+    checkAuth();
+  }, [authenticateUser]);
+  if(isLoading) {
+    return
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoutes;
