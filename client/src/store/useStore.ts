e59456e445a6ef592230c13f8ce6e86c5924f874
Prash766 <prashantshukla766@gmail.com>
2024-12-11 01:32:07 +0530

    added send message function

--- client/src/store/useStore.ts
+++ client/src/store/useStore.ts
@@ -1,58 +1,65 @@
-import axiosClient from '@/axiosClient'
-import { toast } from 'sonner'
-import { create } from 'zustand'
-import { UserType } from './useAuthStore'
+import axiosClient from "@/axiosClient";
+import { toast } from "sonner";
+import { create } from "zustand";
+import { UserType } from "./useAuthStore";
 
 interface ChatStoreType {
-    messages: any[],
-    users: any[],
-    selectedUser: UserType | null,
-    isUsersLoading: boolean,
-    isMessagesLoading: boolean
-    getUsers :()=> Promise<any>
-    getMessages: (userId: string)=> Promise<any>
-    setSelectedUser : (selectedId :UserType) => void
+  messages: any[];
+  users: any[];
+  selectedUser: UserType | null;
+  isUsersLoading: boolean;
+  isMessagesLoading: boolean;
+  getUsers: () => Promise<any>;
+  getMessages: (userId: string) => Promise<any>;
+  setSelectedUser: (selectedId: UserType) => void;
+  sendMessages: (messageData: any) => Promise<any>;
 }
 
-export const useChatStore = create<ChatStoreType>((set) => ({
-    messages: [],
-    users: [],
-    selectedUser: null,
-    isUsersLoading: false,
-    isMessagesLoading: false,
+export const useChatStore = create<ChatStoreType>((set, get) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
 
-    getUsers: async () => {
-        set({ isUsersLoading: true })
-        try {
-            const res = await axiosClient.get('/messages/users')
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosClient.get("/messages/users");
 
-            set({ users: res.data.filteredUsers })
+      set({ users: res.data.filteredUsers });
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
 
-        } catch (error: any) {
-            console.log(error)
-            toast.error(error.message)
-        }
-        finally {
-            set({ isUsersLoading: false })
-        }
-    },
-
-    getMessages: async (userId: string) => {
-        set({ isMessagesLoading: true })
-        try {
-            const res = await axiosClient.get(`/messages/${userId}`)
-            set({ messages: res.data.messages })
-
-        } catch (error: any) {
-            toast.error(error.response.data.message)
-
-        }
-        finally {
-            set({ isMessagesLoading: false })
-        }
-    },
-    setSelectedUser: (selectedUser)=> set({selectedUser : selectedUser}) 
-
-}
-)
-)
+  getMessages: async (userId: string) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosClient.get(`/messages/${userId}`);
+      set({ messages: res.data.messages });
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  setSelectedUser: (selectedUser) => set({ selectedUser: selectedUser }),
+  sendMessages: async (messageData: any) => {
+    const { messages, selectedUser } = get();
+    try {
+      const res = await axiosClient.post(
+        `/messages/send/${selectedUser?._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data.message] });
+      return res
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  },
+}));
